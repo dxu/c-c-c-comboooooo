126eee7cbf072d704a567fae9ebf230a7b774489
David <xudavidb@gmail.com>
2015-10-04 23:28:56 -0700

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
-var gulp = require("gulp"),
-  , sourcemaps = require("gulp-sourcemaps"),
-  , babel = require("gulp-babel"),
-  , concat = require("gulp-concat");
+const gulp = require('gulp')
+  , babel = require('gulp-babel')
+  , concat = require('gulp-concat')
+  , sourcemaps = require('gulp-sourcemaps')
+  , watch = require('gulp-watch');
 
-gulp.task("default", function () {
-  return gulp.src("src/**/*.js")
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(concat("c-c-c-combo.js"))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist"));
+gulp.task('default', function() {
+  watch('src/**/*.js', function() {
+    gulp.src('src/**/*.js')
+      .pipe(sourcemaps.init())
+      .pipe(babel())
+      .pipe(concat('c-c-c-combo.js'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('dist'));
+  });
 });
