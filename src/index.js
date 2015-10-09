3a09ac28d2fcd68d862c96cc6d99feacd07a4104
David <xudavidb@gmail.com>
2015-10-09 11:17:53 -0700

    stuff

--- src/index.js
+++ src/index.js
@@ -19,13 +19,22 @@ ComboStack.prototype.push = function(value) {
 
 // values is an array
 ComboStack.prototype.pull = function(values) {
-  // search for values and pull it out of the stack
+  var index, valuesArray;
+  // search for values and pull it out of the stack, and also pull it out of
+  // values
+  // SEARCH STARTING FROM END - THE MOST RECENT ONE FIRST
+  // convert values
+  valuesArray = typeof values === 'Array' ? values : [values]
 
   // if it's multiple values, then you should return an array.
   // if it's a single value, you should return a single value.
+  for(var i=this.contents.length; i--;) {
+    if (index = values.indexOf(this.contents[i])) {
+      returnArray.push(values.splice(index, 1))
 
-
-  return []
+    }
+  }
+  return returnArray
 }
 
 ComboStack.prototype.isEmpty = function() {
@@ -52,6 +61,11 @@ ComboStack.prototype.empty = function() {
   this.contents = [];
 }
 
+//
+// You should only be able to have a maximum depth of 2 - anytime you lift your
+// finger you are incurring another "step". only when all your fingers have been
+// lifted should the countdown timer start. The keup interval determines
+// whether or not your combination was merged together
 function addCombinationEventListener(el, callback) {
   let stack = new ComboStack()
     , currentCombination = new ComboStack()
@@ -63,6 +77,9 @@ function addCombinationEventListener(el, callback) {
     callback(evt.detail)
   });
 
+  // checks your keydown event. if after INTERVAL amount of time, there are no
+  // more keypresses, and there is nothing in the current combination, then
+  // you're done with the combination
   el.addEventListener('keydown', (evt) => {
     // each time you trigger a keydown, reset the interval,
     window.clearTimeout(interval);
@@ -76,18 +93,34 @@ function addCombinationEventListener(el, callback) {
         callback(stack.contents)
         // reset the stack for the future
         stack = new ComboStack()
+      // otherwise we continue trying to detect
       } else {
         interval = window.setTimeout(newCombination, INTERVAL)
       }
     }, INTERVAL);
-  });
+  })
 
+  // checks your keyup. Each time you keyup, you want to check the keyup
+  // interval. if the keyup interval hasn't cleared yet, that means if you
+  // do more keyups, they should count as "held down together". if the keyup
+  // interval has cleared, you should push the keyup onto the stack, and
+  // remove it from the current combination.
+  //
+  // edge case: very quickly press down, lift up, and then press down again
+  // solution: keydown should somehow detect and remove the keyup and push it
+  // onto the stack - BUT IS IT AN EDGE CASE???????? in the case of two different
+  // keys, for example D, + E, LIFT D, LIFT E, maybe it SHOULD be considered D + E
+  // HOWEVER if you do D, + E, LIFT E, PRESS E QUICKLY< LIFT E, then it should be considered D, E, E, E
+  // HOWEVER, if you do D, +E, LIFT E, LIFT D, then it should be considered D + E
+  // however, this should probably be handled already...
   el.addEventListener('keyup', evt => {
     // immediately push it to the stack if the most recent keydown was the same keyup,
     // because that means that they pressed the button and released
-    if (currentCombination.peek() === evt.keyCode) {
+    // if (currentCombination.peek() === evt.keyCode) {
+    //   stack.push(currentCombination.pull(evt.keyCode))
+      // FIXME: SHOULD NOT DO THIS BECAUSE MAYBE IF YOU LIFT THE OTHERS REAL QUICK IT SHOULD BE ONE TOGETHER
 
-    }
+    // }
     window.clearTimeout(keyupInterval)
     // clear the exiting keyupInterval
     // when keyup, push to keyupStack.
@@ -97,10 +130,7 @@ function addCombinationEventListener(el, callback) {
     keyupInterval = window.setTimeout(function() {
       stack.push(currentCombination.pull(keyupStack.contents))
     })
-
-
   })
-
 }
 
 
