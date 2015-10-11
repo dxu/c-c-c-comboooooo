c7686f42655cd13da9e084dcf803da70d68a4976
David <xudavidb@gmail.com>
2015-10-10 20:59:27 -0700

    set up es6 module support with browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,46 @@
 const gulp = require('gulp')
-  , babel = require('gulp-babel')
   , concat = require('gulp-concat')
   , sourcemaps = require('gulp-sourcemaps')
-  , watch = require('gulp-watch');
+  , source = require('vinyl-source-stream')
+  , buffer = require('vinyl-buffer')
+  , watch = require('gulp-watch')
+  , plumber = require('gulp-plumber')
+  , browserify = require('browserify')
+  , util = require('gulp-util')
+  , watchify = require('watchify')
+  , babelify = require('babelify');
+
+
+function compile(watch) {
+  const bundler =
+    watchify(
+      browserify('./src/index.js', { debug: true })
+        // for es6 module support
+        .add(require.resolve('babelify/polyfill'))
+        .transform(babelify)
+    )
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', util.log.bind(util, 'Browserify Error'))
+      .pipe(plumber())
+      .pipe(source('c-c-c-combo.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./dist'))
+  }
+
+  rebundle()
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('*** Changes detected! Rebundling...');
+      rebundle();
+    });
+  }
+}
 
 gulp.task('default', function() {
-  watch('src/**/*.js', function() {
-    gulp.src('src/**/*.js')
-      .pipe(sourcemaps.init())
-      .pipe(babel())
-      .pipe(concat('c-c-c-combo.js'))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('dist'));
-  });
+  compile(true)
 });
