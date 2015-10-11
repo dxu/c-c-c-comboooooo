4e3250f06b36b8267ba6ddfbc8176e3aa5a7317b
David <xudavidb@gmail.com>
2015-10-10 21:52:34 -0700

    add node tests

---
+++ tests/node/combostack.test.js
@@ -0,0 +1,10 @@
+import { ComboStack } from './src/ComboStack'
+
+describe('ComboStack test suite', function () {
+  it('should pass an empty test', function (done) {
+    done();
+  });
+  it('should be able to create an empty ComboStack', function (done) {
+    done();
+  });
+});
