693318bc5f6bf7f81244c62779530e4185a76be5
David <xudavidb@gmail.com>
2015-10-10 21:52:28 -0700

    separate browser tests

--- test/test.js
+++
@@ -1,7 +0,0 @@
-
-(() => console.log('hello'))();
-$(function(){
-  addCombinationEventListener($('#test')[0], function(result){
-    console.log('result', result)
-  })
-})
---
+++ tests/browser/unit/combo.test.js
@@ -0,0 +1,5 @@
+describe('ComboStack test suite', function () {
+  it('should pass an empty test', function (done) {
+    done();
+  });
+});
