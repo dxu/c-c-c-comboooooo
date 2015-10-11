baeecb03da97143d674a41076b147470981e1925
David <xudavidb@gmail.com>
2015-10-10 23:41:01 -0700

    update pull test on multiple values

--- tests/node/combostack.test.js
+++ tests/node/combostack.test.js
@@ -44,10 +44,8 @@ describe('ComboStack test suite', function () {
   it('should be able to remove a single value from an empty ComboStack without modifying the array or erroring', function (done) {
     var combo, pulledResult
     combo = new ComboStack()
-    combo.push('1')
     pulledResult = combo.pull('1')
     combo.contents.length.should.equal(0)
-    pulledResult.should.deep.equal(['1'])
     done();
   });
 
@@ -70,6 +68,23 @@ describe('ComboStack test suite', function () {
     combo.contents.should.deep.equal(['1'])
     combo.contents.length.should.equal(1)
     pulledResult.should.deep.equal([1])
+    done();
+  });
+
+  it('should be able to remove an array of multiple values in any order from a ComboStack with multiple values and return an array with those values', function (done) {
+    var combo, pulledResult
+    combo = new ComboStack()
+    combo.push('1')
+    combo.push(1)
+    combo.push('2')
+    combo.push(2)
+    combo.push('3')
+    combo.push(3)
+    pulledResult = combo.pull([3, '2'])
+    combo.contents.should.deep.equal(['1', 1, 2, '3'])
+    combo.contents.length.should.equal(4)
+    // is it necessary to be in the same order?!
+    pulledResult.should.deep.equal([3, '2'])
     done();
   });
 
