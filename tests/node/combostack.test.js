dd7196dd1aa484b773265816f20c1c83bd1f44ef
David <xudavidb@gmail.com>
2015-10-11 17:40:25 -0700

    add extra pull tests

--- tests/node/combostack.test.js
+++ tests/node/combostack.test.js
@@ -68,16 +68,14 @@ describe('ComboStack test suite', function () {
   /* Pulling Existing Values */
 
   it('should be able to remove a single value from a ComboStack with one value and return an array with that one value', function (done) {
-    var pulledResult
-    pulledResult = fixtureSingleStack.pull('1')
+    var pulledResult = fixtureSingleStack.pull('1')
     fixtureEmptyStack.contents.length.should.equal(0)
     pulledResult.should.deep.equal(['1'])
     done();
   });
 
   it('should be able to remove a single value from a ComboStack with multiple values and return an array with that one value', function (done) {
-    var pulledResult
-    pulledResult = fixtureDoubleStack.pull(1)
+    var pulledResult = fixtureDoubleStack.pull(1)
     fixtureDoubleStack.contents.should.deep.equal(['1'])
     fixtureDoubleStack.contents.length.should.equal(1)
     pulledResult.should.deep.equal([1])
@@ -85,8 +83,7 @@ describe('ComboStack test suite', function () {
   });
 
   it('should be able to remove a single value as an array from a ComboStack with multiple values and return an array with that one value', function (done) {
-    var pulledResult
-    pulledResult = fixtureDoubleStack.pull([1])
+    var pulledResult = fixtureDoubleStack.pull([1])
     fixtureDoubleStack.contents.should.deep.equal(['1'])
     fixtureDoubleStack.contents.length.should.equal(1)
     pulledResult.should.deep.equal([1])
@@ -94,8 +91,7 @@ describe('ComboStack test suite', function () {
   });
 
   it('should be able to remove an array of multiple values in any order from a ComboStack with multiple values and return an array with those values', function (done) {
-    var pulledResult
-    pulledResult = fixtureMultipleStack.pull([3, '2'])
+    var pulledResult = fixtureMultipleStack.pull([3, '2'])
     fixtureMultipleStack.contents.should.deep.equal(['1', 1, 2, '3'])
     fixtureMultipleStack.contents.length.should.equal(4)
     // TODO/THINK: should it be necessary to be in the same order?!
@@ -103,18 +99,32 @@ describe('ComboStack test suite', function () {
     done();
   });
 
+  it('should be able to pull everything', function (done) {
+    var pulledResult = fixtureMultipleStack.pull(['1', 1, '2', 2, '3', 3])
+    fixtureMultipleStack.contents.length.should.equal(0)
+    // TODO/THINK: should it be necessary to be in the same order?!
+    pulledResult.should.deep.equal(['1', 1, '2', 2, '3', 3])
+    done();
+  });
+
+  it('should be able to pull more than there is available', function (done) {
+    var pulledResult = fixtureMultipleStack.pull(['1', 1, '2', 2, '3', 3, '4', 4])
+    fixtureMultipleStack.contents.length.should.equal(0)
+    // TODO/THINK: should it be necessary to be in the same order?!
+    pulledResult.should.deep.equal(['1', 1, '2', 2, '3', 3])
+    done();
+  });
+
   /* Pull Non-existant Values */
 
   it('should be able to remove a single value from an empty ComboStack without modifying the array or erroring', function (done) {
-    var pulledResult
-    pulledResult = fixtureEmptyStack.pull('1')
+    var pulledResult = fixtureEmptyStack.pull('1')
     fixtureEmptyStack.contents.length.should.equal(0)
     done();
   });
 
   it('should be able to try to remove an empty array passed in without erroring', function (done) {
-    var pulledResult
-    pulledResult = fixtureMultipleStack.pull(undefined)
+    var pulledResult = fixtureMultipleStack.pull([])
     fixtureMultipleStack.contents.should.deep.equal(['1', 1, '2', 2, '3', 3])
     fixtureMultipleStack.contents.length.should.equal(6)
     // TODO/THINK: should it be necessary to be in the same order?!
@@ -123,8 +133,7 @@ describe('ComboStack test suite', function () {
   });
 
   it('should be able to try to remove undefined without erroring', function (done) {
-    var pulledResult
-    pulledResult = fixtureMultipleStack.pull([])
+    var pulledResult = fixtureMultipleStack.pull(undefined)
     fixtureMultipleStack.contents.should.deep.equal(['1', 1, '2', 2, '3', 3])
     fixtureMultipleStack.contents.length.should.equal(6)
     // TODO/THINK: should it be necessary to be in the same order?!
@@ -133,8 +142,7 @@ describe('ComboStack test suite', function () {
   });
 
   it('should be able to try to remove a single value that does not exist', function (done) {
-    var pulledResult
-    pulledResult = fixtureMultipleStack.pull(4)
+    var pulledResult = fixtureMultipleStack.pull(4)
     fixtureMultipleStack.contents.should.deep.equal(['1', 1, '2', 2, '3', 3])
     fixtureMultipleStack.contents.length.should.equal(6)
     // TODO/THINK: should it be necessary to be in the same order?!
@@ -143,8 +151,7 @@ describe('ComboStack test suite', function () {
   });
 
   it('should be able to try to remove a single value passed as an array that does not exist', function (done) {
-    var pulledResult
-    pulledResult = fixtureMultipleStack.pull([4])
+    var pulledResult = fixtureMultipleStack.pull([4])
     fixtureMultipleStack.contents.should.deep.equal(['1', 1, '2', 2, '3', 3])
     fixtureMultipleStack.contents.length.should.equal(6)
     // TODO/THINK: should it be necessary to be in the same order?!
@@ -154,9 +161,8 @@ describe('ComboStack test suite', function () {
 
   /* Getting Leftovers (returnUnsuccessful = true) */
 
-  it('should be able to try to remove a single value passed as an array that does not exist', function (done) {
-    var pulledResult
-    pulledResult = fixtureMultipleStack.pull([4], true)
+  it('should be able to get the leftovers of trying to remove a single value passed as an array that does not exist', function (done) {
+    var pulledResult = fixtureMultipleStack.pull([4], true)
     fixtureMultipleStack.contents.should.deep.equal(['1', 1, '2', 2, '3', 3])
     fixtureMultipleStack.contents.length.should.equal(6)
     // TODO/THINK: should it be necessary to be in the same order?!
@@ -164,9 +170,8 @@ describe('ComboStack test suite', function () {
     done();
   });
 
-  it('should be able to try to remove a single value that does not exist', function (done) {
-    var pulledResult
-    pulledResult = fixtureMultipleStack.pull(4, true)
+  it('should be able to get the leftovers of trying to remove a single value that does not exist', function (done) {
+    var pulledResult = fixtureMultipleStack.pull(4, true)
     fixtureMultipleStack.contents.should.deep.equal(['1', 1, '2', 2, '3', 3])
     fixtureMultipleStack.contents.length.should.equal(6)
     // TODO/THINK: should it be necessary to be in the same order?!
@@ -174,9 +179,8 @@ describe('ComboStack test suite', function () {
     done();
   });
 
-  it('should be able to try to remove multiple values that do exist', function (done) {
-    var pulledResult
-    pulledResult = fixtureMultipleStack.pull([1, 2], true)
+  it('should be able to get the leftovers of trying to remove multiple values that do exist', function (done) {
+    var pulledResult = fixtureMultipleStack.pull([1, 2], true)
     fixtureMultipleStack.contents.should.deep.equal(['1', '2', '3', 3])
     fixtureMultipleStack.contents.length.should.equal(4)
     // TODO/THINK: should it be necessary to be in the same order?!
@@ -184,9 +188,8 @@ describe('ComboStack test suite', function () {
     done();
   });
 
-  it('should be able to try to remove a mix of multiple values that do and do not exist', function (done) {
-    var pulledResult
-    pulledResult = fixtureMultipleStack.pull([1, 4], true)
+  it('should be able to get the leftovers of trying to remove a mix of multiple values that do and do not exist', function (done) {
+    var pulledResult = fixtureMultipleStack.pull([1, 4], true)
     fixtureMultipleStack.contents.should.deep.equal(['1', '2', 2, '3', 3])
     fixtureMultipleStack.contents.length.should.equal(5)
     // TODO/THINK: should it be necessary to be in the same order?!
@@ -194,6 +197,30 @@ describe('ComboStack test suite', function () {
     done();
   });
 
+  it('should be able to get the leftovers of trying to remove everything', function (done) {
+    var pulledResult = fixtureMultipleStack.pull(['1', 1, '2', 2, '3', 3], true)
+    fixtureMultipleStack.contents.length.should.equal(0)
+    // TODO/THINK: should it be necessary to be in the same order?!
+    pulledResult.should.deep.equal([])
+    done();
+  });
 
+  it('should be able to get the leftovers after pulling more than there is available', function (done) {
+    var pulledResult = fixtureMultipleStack.pull(['1', 1, '2', 2, '3', 3, '4', 4], true)
+    fixtureMultipleStack.contents.length.should.equal(0)
+    // TODO/THINK: should it be necessary to be in the same order?!
+    pulledResult.should.deep.equal(['4', 4])
+    done();
+  });
+
+  /* Peeking */
+
+
+  /* Emptying */
+
+  it('should be able empty an empty ComboStack without erroring', function (done) {
+
+    done();
+  });
 
 });
