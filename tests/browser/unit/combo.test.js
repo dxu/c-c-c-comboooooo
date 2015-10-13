36ef83bb8061bbaf9768163d4e869b962ed90994
David <xudavidb@gmail.com>
2015-10-13 11:04:52 -0700

    add keypress helper functions

--- tests/browser/unit/combo.test.js
+++ tests/browser/unit/combo.test.js
@@ -2,8 +2,15 @@ describe('ComboStack test suite', function () {
   var fixtureInputElement
     , QUICK_INTERVAL = 50 // used to simulate "holding down"
     , SLOW_INTERVAL = 150 // used to simulate a quick individual keypress
+    , SEPARATE_INTERVAL = 250 // used to simulate a slow individual keypress
+                              // (separate from the previous keypress)
+    , KEYCODE_A = 65
+    , KEYCODE_B = 66
+    , KEYCODE_C = 67
+    , KEYCODE_D = 68
 
-  function generateKeyEvent(element, keyCode, eventName) {
+
+  function _generateKeyEvent(element, keyCode, eventName) {
     // KeyboardEvent apparently not supported in phantomjs.
     var e = document.createEvent('Event')
     e.keyCode = keyCode
@@ -14,10 +21,48 @@ describe('ComboStack test suite', function () {
     element.dispatchEvent(e)
   }
 
-  function generateKeyboardDownUp(element, keyCode) {
-    generateKeyEvent(fixtureInputElement, keyCode, 'keydown')
-    generateKeyEvent(fixtureInputElement, keyCode, 'keyup')
+  function _generateKeyDownUpEvent(element, keyCode) {
+    _generateKeyEvent(fixtureInputElement, keyCode, 'keydown')
+    window.setTimeout(
+      _generateKeyEvent.bind(this, fixtureInputElement, keyCode, 'keyup')
+    , QUICK_INTERVAL)
   }
+
+  // execute a keydown after timeout
+  Object.prototype._keyDown = function _keyDown(element, keyCode, timeout) {
+    timeout = timeout || 0
+    return new Promise(function(resolve, reject) {
+      window.setTimeout(function() {
+        _generateKeyEvent(element, keyCode, 'keydown')
+        resolve()
+      }, timeout)
+    })
+  }
+
+  // execute an immediate keyup
+  Object.prototype._keyUp = function _keyUp(element, keyCode, timeout) {
+    timeout = timeout || 0
+    return new Promise(function(resolve, reject) {
+      window.setTimeout(function() {
+        _generateKeyEvent(element, keyCode, 'keyup')
+        resolve()
+      }, timeout
+      )
+    })
+  }
+
+  // execute an immediate keyDownUp
+  function _keyDownUp(element, keyCode, timeout) {
+    timeout = timeout || 0
+    console.log(timeout)
+    return new Promise(function(resolve, reject) {
+      window.setTimeout(function() {
+        _generateKeyDownUpEvent(element, keyCode, 'keyup')
+        resolve()
+      }, timeout)
+    })
+  }
+
 
   beforeEach(function(done) {
     fixtureInputElement = document.createElement('input')
@@ -31,22 +76,11 @@ describe('ComboStack test suite', function () {
   it('should pass single keypresses', function (done) {
 
     addCombinationEventListener(fixtureInputElement, function(result) {
-      result.should.deep.equal([1])
+      result.should.deep.equal([KEYCODE_A])
       done()
     })
-    generateKeyboardDownUp(fixtureInputElement, 1)
+    _keyDownUp(fixtureInputElement, KEYCODE_A)
   })
 
-  it('should pass multiple, spaced, single keypresses', function (done) {
-    addCombinationEventListener(fixtureInputElement, function(result) {
-      result.should.deep.equal([[1, 1]])
-      done()
-    })
-    generateKeyboardDownUp(fixtureInputElement, 1)
-
-    window.setTimeout(function() {
-    })
-    generateKeyboardDownUp(fixtureInputElement, 1)
-  })
 
 });
