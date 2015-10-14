a91dc88d7bf177590760da39cc4255c1a4281560
David <xudavidb@gmail.com>
2015-10-13 19:18:22 -0700

    add multiple presses

--- tests/browser/unit/combo.test.js
+++ tests/browser/unit/combo.test.js
@@ -92,7 +92,7 @@ describe('ComboStack test suite', function () {
     })
   })
 
-  it('should pass multiple keypresses of 2 different keys together', function (done) {
+  it('should pass keypresses of 2 different keys together', function (done) {
     addCombinationEventListener(fixtureInputElement, function(result) {
       result.should.deep.equal([[KEYCODE_A, KEYCODE_B]])
       done()
@@ -103,7 +103,7 @@ describe('ComboStack test suite', function () {
       .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_B))
   })
 
-  it('should pass 2 spaced single keypresses of the same letter within the same combination', function(done) {
+  it('should pass 2 single keypresses of the same letter within the same combination', function(done) {
     addCombinationEventListener(fixtureInputElement, function(result) {
       result.should.deep.equal([KEYCODE_A, KEYCODE_A])
       done()
@@ -124,5 +124,76 @@ describe('ComboStack test suite', function () {
       .then(_keyDown.bind(null, fixtureInputElement, KEYCODE_A, SLOW_INTERVAL))
       .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_A))
   })
+
+  it('should pass a keypress of two letters A & B, and then multiple single keypresses of button A and button B', function(done) {
+    addCombinationEventListener(fixtureInputElement, function(result) {
+      result.should.deep.equal([[KEYCODE_A, KEYCODE_B], KEYCODE_B, KEYCODE_A, KEYCODE_B])
+      done()
+    })
+    _keyDown(fixtureInputElement, KEYCODE_A)
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_A))
+      .then(_keyDown.bind(null, fixtureInputElement, KEYCODE_B))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_B))
+      .then(_keyDownUp.bind(null, fixtureInputElement, KEYCODE_B, SLOW_INTERVAL))
+      .then(_keyDownUp.bind(null, fixtureInputElement, KEYCODE_A, SLOW_INTERVAL))
+      .then(_keyDownUp.bind(null, fixtureInputElement, KEYCODE_B, SLOW_INTERVAL))
+  })
+
+  it('should pass multiple single keypresses of button A and button B, and then a keypress of two letters A & B', function(done) {
+    addCombinationEventListener(fixtureInputElement, function(result) {
+      result.should.deep.equal([KEYCODE_B, KEYCODE_A, KEYCODE_B, [KEYCODE_A, KEYCODE_B]])
+      done()
+    })
+    _keyDownUp(fixtureInputElement, KEYCODE_B)
+      .then(_keyDownUp.bind(null, fixtureInputElement, KEYCODE_A, SLOW_INTERVAL))
+      .then(_keyDownUp.bind(null, fixtureInputElement, KEYCODE_B, SLOW_INTERVAL))
+      .then(_keyDown.bind(null, fixtureInputElement, KEYCODE_A, SLOW_INTERVAL))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_A))
+      .then(_keyDown.bind(null, fixtureInputElement, KEYCODE_B))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_B))
+  })
+
+  it('should pass multiple spaced simultaneous A & B keypresses', function(done) {
+    addCombinationEventListener(fixtureInputElement, function(result) {
+      result.should.deep.equal([[KEYCODE_A, KEYCODE_B], [KEYCODE_A, KEYCODE_B], [KEYCODE_A, KEYCODE_B], [KEYCODE_A, KEYCODE_B]])
+      done()
+    })
+    _keyDown(fixtureInputElement, KEYCODE_A)
+      .then(_keyDown.bind(null, fixtureInputElement, KEYCODE_B))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_A))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_B))
+      .then(_keyDown.bind(null, fixtureInputElement, KEYCODE_A, SLOW_INTERVAL))
+      .then(_keyDown.bind(null, fixtureInputElement, KEYCODE_B))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_A))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_B))
+      .then(_keyDown.bind(null, fixtureInputElement, KEYCODE_A, SLOW_INTERVAL))
+      .then(_keyDown.bind(null, fixtureInputElement, KEYCODE_B))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_A))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_B))
+      .then(_keyDown.bind(null, fixtureInputElement, KEYCODE_A, SLOW_INTERVAL))
+      .then(_keyDown.bind(null, fixtureInputElement, KEYCODE_B))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_A))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_B))
+  })
+
+  it('should pass if you just hold A down for a really long time, while continuing to press B, and lifting A at the end', function(done) {
+    addCombinationEventListener(fixtureInputElement, function(result) {
+      result.should.deep.equal([KEYCODE_B, KEYCODE_B, KEYCODE_B, KEYCODE_B, KEYCODE_B, KEYCODE_A])
+      done()
+    })
+    _keyDown(fixtureInputElement, KEYCODE_A)
+      .then(_keyDownUp.bind(null, fixtureInputElement, KEYCODE_B))
+      .then(_keyDownUp.bind(null, fixtureInputElement, KEYCODE_B, SLOW_INTERVAL))
+      .then(_keyDownUp.bind(null, fixtureInputElement, KEYCODE_B, SLOW_INTERVAL))
+      .then(_keyDownUp.bind(null, fixtureInputElement, KEYCODE_B, SLOW_INTERVAL))
+      .then(_keyDownUp.bind(null, fixtureInputElement, KEYCODE_B, SLOW_INTERVAL))
+      .then(_keyUp.bind(null, fixtureInputElement, KEYCODE_A, SEPARATE_INTERVAL))
+  })
+
+  it('should pass if ')
+
+
+
+
 
 });
